8b6f67b4067b6fcedcc19a6d392e32c42dbe4c52
Gavassinha9 <134518907+Gavassinha9@users.noreply.github.com>
2023-08-02 12:42:36 -0300

    Add files via upload

---
+++ aula js/forms.js
@@ -0,0 +1,10 @@
+function Enviar(){
+     var nome = document.getElementById("nomeid");
+     if(nome.value !=""){
+        alert('Obrigado sr(a)' + nome.value + 'seu formulário foi enviado e descartado')
+     }
+     else{
+        alert('whats is this')
+     }
+
+}
